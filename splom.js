7c91f24abfcd7756d1f013c24774b7032836270f
Barr Iserloth <barriserloth@gmail.com>
2017-04-23 12:01:00 -0500

    Added more splom formatting/gridlines

--- splom.js
+++ splom.js
@@ -1,12 +1,5 @@
-// Part of a brief D3 tutorial.
-// Upon completion, will display an interactive scatterplot showing relationship between
-//   different values associated with the top 100 words in Shakespeare's First Folio
 // CS 314, Spring 2017
 // Eric Alexander
-
-// https://strongriley.github.io/d3/ex/splom.html
-// cite that ^
-
 
 // First, we will create some constants to define non-data-related parts of the visualization
 w = 300; // Width of our visualization
@@ -14,6 +7,9 @@ h = 250; // Height of our visualization
 xOffset = 20; // Space for x-axis labels
 yOffset = 50; // Space for y-axis labels
 margin = 10; // Margin around visualization
+border=1;
+bordercolor='black';
+
 vals = ['flight_index', 'num_o_ring_distress', 'launch_temp',
   'leak_check_pressure', 'tufte_metric'
 ];
@@ -22,13 +18,26 @@ labels = ['Flight Index', 'O-Ring Distress', 'Launch Temp',
 
 var color = d3.scale.category10();
 
+function make_x_axis(){
+  return d3.svg.axis()
+    .scale(xScale)
+    .orient('bottom')
+    .ticks(5)
+}
+
+function make_y_axis(){
+  return d3.svg.axis()
+    .scale(yScale)
+    .orient('left')
+    .ticks(5)
+}
+
 // Next, we will load in our CSV of data
 d3.csv('challenger.csv', function(data) {
   for(i=0; i<5; i++)
     for(j=0; j<5; j++)
       plot(data, vals[j], vals[i], labels[i]);
 });
-
 
 function plot(data, xVal, yVal, xName){
 
@@ -40,7 +49,7 @@ function plot(data, xVal, yVal, xName){
         return parseFloat(d[xVal]);
       })
     ])
-    .range([yOffset + margin, w-margin]);
+    .range([margin+yOffset, w-margin*4]);
   yScale = d3.scale.linear()
     .domain([d3.min(data, function(d) {
         return parseFloat(d[yVal]);
@@ -49,41 +58,96 @@ function plot(data, xVal, yVal, xName){
         return parseFloat(d[yVal]);
       })
     ])
-    .range([h-xOffset-margin, margin]); // Notice this is backwards!
+    .range([h-xOffset-margin, margin*3.5]); // Notice this is backwards!
 
 
   // Next, we will create an SVG element to contain our visualization.
   svg = d3.select('#pointsSVG').append('svg:svg')
     .attr('width', w)
     .attr('height', h)
+    .attr('border', border);
 
-  // Build axes! (These are kind of annoying, actually...)
   if(xVal !== yVal){
+    svg.append('g')
+       .attr('class', 'grid')
+       .attr('transform', 'translate(0,' + h + ')')
+       .call(make_x_axis()
+	    .tickSize(-h, 0, 0)
+	    .tickFormat('')
+       )
+
+	    svg.append('g')
+	       .attr('class', 'grid')
+	       .call(make_y_axis()
+		   .tickSize(-w, 0, 0)
+		   .tickFormat('')
+	       )
+       
+  }
+
+  if(xVal === yVal){
+    svg.style('background-color', 'gray');
+  }
+
+  borderPath = svg.append('rect')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('height', h)
+    .attr('width', w)
+    .style('stroke', bordercolor)
+    .style('fill', 'none')
+    .style('stroke-width', border);
+  
+  // Build axes! (These are kind of annoying, actually...)
+  if(xVal !== yVal && i == 0){
+    xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient('top')
+      .ticks(5);
+    xAxisG = svg.append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(0,' + 30 + ')')
+      .call(xAxis);
+  }
+
+  if(xVal !== yVal && i == 4){
     xAxis = d3.svg.axis()
       .scale(xScale)
       .orient('bottom')
-      .ticks(10);
+      .ticks(5);
     xAxisG = svg.append('g')
       .attr('class', 'axis')
       .attr('transform', 'translate(0,' + (h - xOffset) + ')')
       .call(xAxis);
   }
+
   if(xVal === yVal){
     xLabel = svg.append('text')
           .attr('class', 'label')
-          .attr('x', w-125)
+          .attr('x', w/2)
           .attr('y', h/2)
           .text(xName);
   }
 
-  if(xVal !== yVal){
+  if(xVal !== yVal && j == 0){
     yAxis = d3.svg.axis()
       .scale(yScale)
       .orient('left')
-      .ticks(10);
+      .ticks(5);
     yAxisG = svg.append('g')
       .attr('class', 'axis')
       .attr('transform', 'translate(' + yOffset + ',0)')
+      .call(yAxis);
+  }
+
+  if(xVal !== yVal && j == 4){
+    yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient('right')
+      .ticks(5);
+    yAxisG = svg.append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(' + (w-margin*3) + ',0)')
       .call(yAxis);
   }
 
@@ -125,6 +189,7 @@ function plot(data, xVal, yVal, xName){
             .style('top', (d3.event.pageY - 14) + 'px');
 
         })
+	// found the d3 'active' attribute to calculate toggles
         .on('click', function(d) {
           var active   = eval('flight_' + d[vals[0]]).active ? false : true;
           d3.selectAll('.flight_' + d[vals[0]])
