2dfcc7d731158c2a440fc87bf81a0f4c07dbaf34
Simon Bilsky-Rollins <simonbr73@gmail.com>
2017-04-24 09:55:53 -0500

    Formatting

--- parallel.js
+++ parallel.js
@@ -10,7 +10,8 @@ vals = ['flight_index', 'num_o_ring_distress', 'launch_temp',
   'leak_check_pressure', 'tufte_metric'
 ];
 labels = ['Flight Index', 'O-Ring Distress', 'Launch Temp',
-'Leak Pressure', 'Tufte Metric'];
+  'Leak Pressure', 'Tufte Metric'
+];
 
 scales = [];
 
@@ -20,57 +21,56 @@ svg = d3.select('body').append('svg')
   .attr('height', h)
 
 d3.csv("challenger.csv", function(data) {
-  for(i=0; i<5; i++){
+  for (i = 0; i < 5; i++) {
     plot_axes(data, vals[i], labels[i], i);
   }
 
   tooltip = d3.select('body').append('div')
-	      .attr('class', 'tooltip')
-	      .style('opacity', 0);
+    .attr('class', 'tooltip')
+    .style('opacity', 0);
 
   lines = svg.append("g")
-      .selectAll("path")
-        .data(data)
-      .enter().append("path")
-        .attr("d", function(d){
-            string = 'M '
-            for(i=0; i<5; i++){
-              string += (margin + 275*i) + ' ';
-              string += ((scales[i](d[vals[i]])));
-              console.log(scales[i](d[vals[i]]));
-              if (i != 4){
-                string += ' L ';
-              }
-            }
-            return string;
-        })
-        .attr('stroke', 'steelblue')
-	.attr('stroke-width', 2)
-        .attr('fill', 'none')
-	.on('mouseover', function(d) {
-	  d3.select(this)
-	    .transition()
-	    .duration(200)
-	    .attr('stroke', 'red');
-	  tooltip.transition()
-	    .duration(200)
-	    .style('opacity', .9)
-	  tooltip.html('Flight ' + d[vals[0]])
-	    .style('left', (d3.event.pageX) + 'px')
-	    .style('top', (d3.event.pageY - 14) + 'px');
-	})
-	.on('mouseout', function(d) {
-	  d3.select(this)
-	    .transition()
-	    .duration(200)
-	    .attr('stroke', 'steelblue');
-	});
+    .selectAll("path")
+    .data(data)
+    .enter().append("path")
+    .attr("d", function(d) {
+      string = 'M '
+      for (i = 0; i < 5; i++) {
+        string += (margin + 275 * i) + ' ';
+        string += ((scales[i](d[vals[i]])));
+        console.log(scales[i](d[vals[i]]));
+        if (i != 4) {
+          string += ' L ';
+        }
+      }
+      return string;
+    })
+    .attr('stroke', 'steelblue')
+    .attr('stroke-width', 2)
+    .attr('fill', 'none')
+    .on('mouseover', function(d) {
+      d3.select(this)
+        .transition()
+        .duration(200)
+        .attr('stroke', 'red');
+      tooltip.transition()
+        .duration(200)
+        .style('opacity', .9)
+      tooltip.html('Flight ' + d[vals[0]])
+        .style('left', (d3.event.pageX) + 'px')
+        .style('top', (d3.event.pageY - 14) + 'px');
+    })
+    .on('mouseout', function(d) {
+      d3.select(this)
+        .transition()
+        .duration(200)
+        .attr('stroke', 'steelblue');
+    });
 
 
 });
 
-function plot_axes(data, xVal, xLabel, i){
-
+function plot_axes(data, xVal, xLabel, i) {
   xScale = d3.scale.linear()
     .domain([d3.min(data, function(d) {
         return parseFloat(d[xVal]);
@@ -79,24 +79,22 @@ function plot_axes(data, xVal, xLabel, i){
         return parseFloat(d[xVal]);
       })
     ])
-    .range([margin, h-20]);
+    .range([margin, h - 20]);
 
-    scales.push(xScale);
+  scales.push(xScale);
 
-    axis = d3.svg.axis()
-      .scale(xScale)
-      .orient('right')
-      .ticks(0)
-      .outerTickSize(0);
-    axisG = svg.append('g')
-      .attr('class', 'axis')
-      .attr('transform', 'translate(' + (margin + (i*space))+ ',0)')
-      .call(axis);
-    label = svg.append('text')
-          .attr('class', 'label')
-          .attr('x', margin+i*space)
-          .attr('y', 20)
-          .text(xLabel);
-
-
+  axis = d3.svg.axis()
+    .scale(xScale)
+    .orient('right')
+    .ticks(0)
+    .outerTickSize(0);
+  axisG = svg.append('g')
+    .attr('class', 'axis')
+    .attr('transform', 'translate(' + (margin + (i * space)) + ',0)')
+    .call(axis);
+  label = svg.append('text')
+    .attr('class', 'label')
+    .attr('x', margin + i * space)
+    .attr('y', 20)
+    .text(xLabel);
 }
--- splom.js
+++ splom.js
@@ -7,25 +7,27 @@ h = 250; // Height of our visualization
 xOffset = 20; // Space for x-axis labels
 yOffset = 50; // Space for y-axis labels
 margin = 10; // Margin around visualization
-border=1;
-bordercolor='black';
+border = 1;
+bordercolor = 'black';
 
 vals = ['flight_index', 'num_o_ring_distress', 'launch_temp',
-  'leak_check_pressure', 'tufte_metric'];
+  'leak_check_pressure', 'tufte_metric'
+];
 
 labels = ['Flight Index', 'O-Ring Distress', 'Launch Temp',
-'Leak Pressure', 'Tufte Metric'];
+  'Leak Pressure', 'Tufte Metric'
+];
 
 var color = d3.scale.category10();
 
-function make_x_axis(){
+function make_x_axis() {
   return d3.svg.axis()
     .scale(xScale)
     .orient('bottom')
     .ticks(5)
 }
 
-function make_y_axis(){
+function make_y_axis() {
   return d3.svg.axis()
     .scale(yScale)
     .orient('left')
@@ -34,12 +36,12 @@ function make_y_axis(){
 
 // Next, we will load in our CSV of data
 d3.csv('challenger.csv', function(data) {
-  for(i=0; i<5; i++)
-    for(j=0; j<5; j++)
+  for (i = 0; i < 5; i++)
+    for (j = 0; j < 5; j++)
       plot(data, vals[j], vals[i], labels[i]);
 });
 
-function plot(data, xVal, yVal, xName){
+function plot(data, xVal, yVal, xName) {
 
   xScale = d3.scale.linear()
     .domain([d3.min(data, function(d) {
@@ -49,7 +51,7 @@ function plot(data, xVal, yVal, xName){
         return parseFloat(d[xVal]);
       })
     ])
-    .range([margin+yOffset, w-margin*4]);
+    .range([margin + yOffset, w - margin * 4]);
   yScale = d3.scale.linear()
     .domain([d3.min(data, function(d) {
         return parseFloat(d[yVal]);
@@ -58,7 +60,7 @@ function plot(data, xVal, yVal, xName){
         return parseFloat(d[yVal]);
       })
     ])
-    .range([h-xOffset-margin, margin*3.5]); // Notice this is backwards!
+    .range([h - xOffset - margin, margin * 3.5]); // Notice this is backwards!
 
 
   // Next, we will create an SVG element to contain our visualization.
@@ -67,23 +69,23 @@ function plot(data, xVal, yVal, xName){
     .attr('height', h)
     .attr('border', border)
 
-  if(xVal !== yVal){
+  if (xVal !== yVal) {
     svg.append('g')
-       .attr('class', 'grid')
-       .attr('transform', 'translate(0,' + h + ')')
-       .call(make_x_axis()
-	    .tickSize(-h, 0, 0)
-	    .tickFormat('')
-       )
+      .attr('class', 'grid')
+      .attr('transform', 'translate(0,' + h + ')')
+      .call(make_x_axis()
+        .tickSize(-h, 0, 0)
+        .tickFormat('')
+      )
 
     svg.append('g')
-       .attr('class', 'grid')
-       .call(make_y_axis()
-	   .tickSize(-w, 0, 0) .tickFormat('')
-       );       
+      .attr('class', 'grid')
+      .call(make_y_axis()
+        .tickSize(-w, 0, 0).tickFormat('')
+      );
   }
 
-  if(xVal === yVal){
+  if (xVal === yVal) {
     svg.style('background-color', 'gray');
   }
 
@@ -95,9 +97,9 @@ function plot(data, xVal, yVal, xName){
     .style('stroke', bordercolor)
     .style('fill', 'none')
     .style('stroke-width', border);
-  
+
   // Build axes! (These are kind of annoying, actually...)
-  if(xVal !== yVal && i == 0){
+  if (xVal !== yVal && i == 0) {
     xAxis = d3.svg.axis()
       .scale(xScale)
       .orient('top')
@@ -108,7 +110,7 @@ function plot(data, xVal, yVal, xName){
       .call(xAxis);
   }
 
-  if(xVal !== yVal && i == 4){
+  if (xVal !== yVal && i == 4) {
     xAxis = d3.svg.axis()
       .scale(xScale)
       .orient('bottom')
@@ -119,15 +121,15 @@ function plot(data, xVal, yVal, xName){
       .call(xAxis);
   }
 
-  if(xVal === yVal){
+  if (xVal === yVal) {
     xLabel = svg.append('text')
-          .attr('class', 'label')
-          .attr('x', w/2)
-          .attr('y', h/2)
-          .text(xName);
+      .attr('class', 'label')
+      .attr('x', w / 2)
+      .attr('y', h / 2)
+      .text(xName);
   }
 
-  if(xVal !== yVal && j == 0){
+  if (xVal !== yVal && j == 0) {
     yAxis = d3.svg.axis()
       .scale(yScale)
       .orient('left')
@@ -138,14 +140,14 @@ function plot(data, xVal, yVal, xName){
       .call(yAxis);
   }
 
-  if(xVal !== yVal && j == 4){
+  if (xVal !== yVal && j == 4) {
     yAxis = d3.svg.axis()
       .scale(yScale)
       .orient('right')
       .ticks(5);
     yAxisG = svg.append('g')
       .attr('class', 'axis')
-      .attr('transform', 'translate(' + (w-margin*3) + ',0)')
+      .attr('transform', 'translate(' + (w - margin * 3) + ',0)')
       .call(yAxis);
   }
 
@@ -153,65 +155,70 @@ function plot(data, xVal, yVal, xName){
   // Create a new div for the tooltip
   // From http://bl.ocks.org/d3noob/a22c42db65eb00d4e369
   tooltip = d3.select('body').append('div')
-              .attr('class', 'tooltip')
-              .style('opacity', 0);
+    .attr('class', 'tooltip')
+    .style('opacity', 0);
 
   // Now, we will start actually building our scatterplot!
   // Select elements
   // Bind data to elements
-  if(xVal !== yVal){
+  if (xVal !== yVal) {
     points = svg.selectAll('circle')
-        .data(data)
+      .data(data)
       .enter()
-        .append('circle')
-        .attr('class', function(d) { return 'flight_' + d[vals[0]]; })
-        .attr('id', function(d) { return 'flight_' + d[vals[0]]; })
-        .attr('cx', function(d) {
-          return xScale(d[xVal]);
-        })
-        .attr('cy', function(d) {
-          return yScale(d[yVal]);
-        })
-        .attr('r', 4)
-        .style('fill', function(d) { return color(d[vals[1]]); })
-        .on('mouseover', function(d) {
-          d3.selectAll('.flight_' + d[vals[0]])
-            .transition()
-            .duration(200)
-            .attr('r', 7);
-          tooltip.transition()
-            .duration(200)
-            .style('opacity', .9)
-          tooltip.html('Flight ' + d[vals[0]] + ': ' + d[xVal] + ', ' + d[yVal])
-            .style('left', (d3.event.pageX) + 'px')
-            .style('top', (d3.event.pageY - 14) + 'px');
+      .append('circle')
+      .attr('class', function(d) {
+        return 'flight_' + d[vals[0]];
+      })
+      .attr('id', function(d) {
+        return 'flight_' + d[vals[0]];
+      })
+      .attr('cx', function(d) {
+        return xScale(d[xVal]);
+      })
+      .attr('cy', function(d) {
+        return yScale(d[yVal]);
+      })
+      .attr('r', 4)
+      .style('fill', function(d) {
+        return color(d[vals[1]]);
+      })
+      .on('mouseover', function(d) {
+        d3.selectAll('.flight_' + d[vals[0]])
+          .transition()
+          .duration(200)
+          .attr('r', 7);
+        tooltip.transition()
+          .duration(200)
+          .style('opacity', .9)
+        tooltip.html('Flight ' + d[vals[0]] + ': ' + d[xVal] + ', ' + d[yVal])
+          .style('left', (d3.event.pageX) + 'px')
+          .style('top', (d3.event.pageY - 14) + 'px');
 
-        })
-	// found the d3 'active' attribute to calculate toggles
-        .on('click', function(d) {
-          var active   = eval('flight_' + d[vals[0]]).active ? false : true;
-          d3.selectAll('.flight_' + d[vals[0]])
-            .transition()
-            .style('fill', function(d){
-                if (active === false){
-                  eval('flight_' + d[vals[0]]).active = active;
-                  return color(d[vals[1]]);
-                }
-                else{
-                  eval('flight_' + d[vals[0]]).active = active;
-                  return 'red';
-                }
-             })
-        })
-        .on('mouseout', function(d) {
-          d3.selectAll('.flight_' + d[vals[0]])
-            .transition()
-            .duration(200)
-            .attr('r', 4);
-          tooltip.transition()
-            .duration(500)
-            .style('opacity', 0);
-        });
+      })
+      // found the d3 'active' attribute to calculate toggles
+      .on('click', function(d) {
+        var active = eval('flight_' + d[vals[0]]).active ? false : true;
+        d3.selectAll('.flight_' + d[vals[0]])
+          .transition()
+          .style('fill', function(d) {
+            if (active === false) {
+              eval('flight_' + d[vals[0]]).active = active;
+              return color(d[vals[1]]);
+            } else {
+              eval('flight_' + d[vals[0]]).active = active;
+              return 'red';
+            }
+          })
+      })
+      .on('mouseout', function(d) {
+        d3.selectAll('.flight_' + d[vals[0]])
+          .transition()
+          .duration(200)
+          .attr('r', 4);
+        tooltip.transition()
+          .duration(500)
+          .style('opacity', 0);
+      });
 
   }
 }
